03867f5322bdbc083e5e6cdb2ddb8d7df85b5300
Adam Esterline <adam@esterlines.com>
2016-02-02 06:00:24 -0700

    The project is now working with figwheel.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :source-paths ["src-clj"]
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[compojure "1.4.0"]
                  [environ "1.0.2"]
                  [http-kit "2.1.18"]
@@ -13,14 +13,69 @@
 
   :plugins [[lein-cljsbuild "1.1.2"]]
 
-  :clean-targets ^{:protect false} [:target-path :compile-path "resources/public/js/main.js"]
+  :clean-targets ^{:protect false} [:target-path :compile-path "resources/public/js/compiled"]
 
   :hooks [leiningen.cljsbuild]
 
-  :cljsbuild {:builds [{:source-paths ["src-cljs"]
-                        :compiler {:output-to "resources/public/js/main.js"
-                                   :optimizations :whitespace
-                                   :pretty-print true}}]}
+  :cljsbuild {:builds
+              {:app
+               {:source-paths ["src-cljs"]
+
+                :figwheel     true
+                ;; Alternatively, you can configure a function to run every time figwheel reloads.
+                ;; :figwheel {:on-jsload "chatapp.core/on-figwheel-reload"}
+
+                :compiler     {:main                 chatapp.core
+                               :asset-path           "js/compiled/out"
+                               :output-to            "resources/public/js/compiled/main.js"
+                               :output-dir           "resources/public/js/compiled/out"
+                               :source-map-timestamp true}}}}
+
+  :figwheel {;; :http-server-root "public"       ;; serve static assets from resources/public/
+             ;; :server-port 3449                ;; default
+             ;; :server-ip "127.0.0.1"           ;; default
+             :css-dirs       ["resources/public/css"] ;; watch and update CSS
+
+             ;; Instead of booting a separate server on its own port, we embed
+             ;; the server ring handler inside figwheel's http-kit server, so
+             ;; assets and API endpoints can all be accessed on the same host
+             ;; and port. If you prefer a separate server process then take this
+             ;; out and start the server with `lein run`.
+             :ring-handler   chatapp.core/routes
+
+             ;; Start an nREPL server into the running figwheel process. We
+             ;; don't do this, instead we do the opposite, running figwheel from
+             ;; an nREPL process, see
+             ;; https://github.com/bhauman/lein-figwheel/wiki/Using-the-Figwheel-REPL-within-NRepl
+             ;; :nrepl-port 7888
+
+             ;; To be able to open files in your editor from the heads up display
+             ;; you will need to put a script on your path.
+             ;; that script will have to take a file path and a line number
+             ;; ie. in  ~/bin/myfile-opener
+             ;; #! /bin/sh
+             ;; emacsclient -n +$2 $1
+             ;;
+             ;; :open-file-command "myfile-opener"
+
+             :server-logfile "log/figwheel.log"}
+
+  :profiles {:dev
+             {:dependencies [[figwheel "0.5.0-4"]
+                             [figwheel-sidecar "0.5.0-4"]
+                             [com.cemerick/piggieback "0.2.1"]
+                             [org.clojure/tools.nrepl "0.2.12"]]
+
+              :plugins      [[lein-figwheel "0.5.0-2"]
+                             [lein-doo "0.1.6"]]
+
+              :cljsbuild    {:builds
+                             {:test
+                              {:source-paths ["src/cljs" "test/cljs"]
+                               :compiler
+                               {:output-to     "resources/public/js/compiled/testable.js"
+                                :main          chatapp.test-runner
+                                :optimizations :none}}}}}}
+
   :main ^:skip-aot chatapp.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :target-path "target/%s")
