1fb4fd0b2cc31db823eb6ed71156b0cb1a298069
Adam Esterline <adam@esterlines.com>
2016-02-07 08:21:17 -0700

    Better styling

--- src/cljs/chatapp/core.cljs
+++ src/cljs/chatapp/core.cljs
@@ -47,35 +47,36 @@
 
 ;; Other stuff
 (defn message [message]
-  [:div
-   [:span {:class (:style message)}
-    (:message message)]])
+  [:li {:class "list-group-item"}
+   (:message message)])
 
 (defn message-list []
   (let [messages (re-frame/subscribe [:messages])]
     (fn []
-      [:div
+      [:ul {:class "list-group"}
        (for [m @messages]
          (message m))])))
 
 (defn demo []
   (let [val (re-frame/subscribe [:message-text])]
     (fn []
-      [:div {:class "row"}
-       [:div {:class "col-lg-6"}
-        [:div {:class "input-group"}
-         [:input {:type        "text"
-                  :value       @val
-                  :placeholder "Enter text to reverse!"
-                  :class       "form-control"
-                  :on-change   #(re-frame/dispatch [:new-message-text (-> % .-target .-value)])}]
-         [:span {:class "input-group-btn"}
-          [:button {:type     "button"
-                    :class    "btn btn-default"
-                    :on-click #(re-frame/dispatch [:send-message])}
-           "Send"]]
-         ]]
-       [message-list]])))
+      [:div {:class "container-fluid"}
+       [:div
+        [message-list]]
+       [:div {:class "row"}
+        [:div {:class "col-lg-6"}
+         [:div {:class "input-group"}
+          [:input {:type        "text"
+                   :value       @val
+                   :placeholder "Enter text to reverse!"
+                   :class       "form-control"
+                   :on-change   #(re-frame/dispatch [:new-message-text (-> % .-target .-value)])}]
+          [:span {:class "input-group-btn"}
+           [:button {:type     "button"
+                     :class    "btn btn-default"
+                     :on-click #(re-frame/dispatch [:send-message])}
+            "Send"]]
+          ]]]])))
 
 (defn start []
   (re-frame/dispatch-sync [:initialise-db])
