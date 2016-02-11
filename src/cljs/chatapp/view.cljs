083b352e29edeb92ed3c9f81b13b709aca1fc1a7
Adam Esterline <adam@esterlines.com>
2016-02-11 05:49:46 -0700

    More playing with css.

--- src/cljs/chatapp/view.cljs
+++ src/cljs/chatapp/view.cljs
@@ -8,11 +8,9 @@
 (defn message-list []
   (let [messages (re-frame/subscribe [:messages])]
     (fn []
-      [:div {:class "row"}
-       [:div {:class "col-sm-6"}
-        [:ul {:class "list-group"}
-         (for [m @messages]
-           (message m))]]])))
+      [:ul {:class "list-group"}
+       (for [m @messages]
+         (message m))])))
 
 (defn message-input []
   (let [input (re-frame/subscribe [:message-input])]
@@ -26,22 +24,20 @@
                :on-change    #(re-frame/dispatch [:message-input-text (-> % .-target .-value)])}])))
 
 (defn message-composer []
-  [:div {:class "row"}
-   [:div {:class "col-sm-6"}
-    [:div {:class "input-group"}
-     [message-input]
-     [:span {:class "input-group-btn"}
-      [:button {:type     "button"
-                :class    "btn btn-default"
-                :on-click #(re-frame/dispatch [:send-message])}
-       "Send"]]]]])
+  [:div {:class "input-group"}
+   [message-input]
+   [:span {:class "input-group-btn"}
+    [:button {:type     "button"
+              :class    "btn btn-default"
+              :on-click #(re-frame/dispatch [:send-message])}
+     "Send"]]])
 
 (defn chat-ui []
   [:div {:class "container pink"}
    [:div {:class "row green"}
     [:div {:class "col-md-12"}
      [:h1 "ChatApp"]]]
-   [:div {:class "row red"}
+   [:div {:class "row red fill"}
     [:div {:class "col-md-9"}
      [message-list]
      [message-composer]]]])
